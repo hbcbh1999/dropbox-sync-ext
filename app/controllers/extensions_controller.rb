f50815f6c81442bff66735d5de64962248221eea
Mo Bitar <me@bitar.io>
2017-01-05 19:40:50 -0600

    actions

--- app/controllers/extensions_controller.rb
+++ app/controllers/extensions_controller.rb
@@ -1,48 +1,96 @@
 class ExtensionsController < ApplicationController
 
+  before_action {
+    if params[:user_uuid]
+      @user = User.find_by_uuid(params[:user_uuid])
+    end
+  }
+
   def show
-    @user = User.find_by_uuid(params[:user_uuid])
+    if !@user
+      render :json => {:error => "Unable to load extension."}
+    end
     name = "Dropbox Sync"
     supported_types = ["Note"]
     actions = [
       {
         :label => "Auto-push changes",
+        :desc => "Pushes note changes to Dropbox folder.",
         :url => "http://localhost:3002/ext/#{@user.uuid}/push",
-        :type => "watch:post:5",
+        :type => "watch:post:7",
         :structures => [
           {
             :type => "Note",
-            :fields => [
-              {
-                :name => "uuid",
-                :modifies => false
-              },
-              {
-                :name => "content.title",
-                :modifies => false
-              },
-              {
-                :name => "content.text",
-                :modifies => false
-              }
-            ]
+            :mode => "read",
+            :decrypted => true
           }
         ]
+      },
+      {
+        :label => "Perform initial sync",
+        :desc => "Syncs all your current notes. This can take several minutes, depending on how many notes you have.",
+        :url => "http://localhost:3002/ext/#{@user.uuid}/initial_sync",
+        :type => "all:post",
+        :structures => [
+          {
+            :type => "Note",
+            :mode => "read",
+            :decrypted => true
+          }
+        ]
+      },
+      {
+        :label => "Download import file",
+        :desc => "Downloads import file in standard format.",
+        :url => "http://localhost:3002/ext/#{@user.uuid}/download",
+        :type => "show"
       }
     ]
     render :json => {:name => name, :supported_types => supported_types, :actions => actions}
   end
 
-  def push
+  def dropbox
     require 'dropbox'
-    @user = User.find_by_uuid(params[:user_uuid])
-    items = params[:items]
     dropbox = Dropbox::Client.new(@user.dropbox_token)
+  end
+
+  def push
+    items = params[:items]
+
+    # write individual files
     items.each do |item|
-      dropbox.upload("/#{item[:uuid]}.txt", "#{item[:content][:title]}\n\n#{item[:content][:text]}", {:mode => "overwrite"})
+      dropbox.upload("/#{item[:uuid]}.txt", "#{JSON.pretty_generate(item.as_json)}", {:mode => "overwrite"})
     end
 
+    # write to master file
+    begin
+      file, body = dropbox.download("/master.txt")
+      contents = JSON.parse(body.to_s)
+    rescue
+      contents = []
+    end
+
+    items.each do |item|
+      existing = contents.find { |e| e["uuid"] == item[:uuid]  }
+      if existing
+        existing.merge!(item.to_unsafe_h)
+      else
+        contents.push(item.to_unsafe_h)
+      end
+    end
+
+    dropbox.upload("/master.txt", JSON.pretty_generate(contents.as_json), {:mode => "overwrite"})
+
     render :json => {:success => true}
+  end
+
+  def initial_sync
+    self.push
+  end
+
+  def download
+    file, body = dropbox.download("/master.txt")
+    send_data body.to_s, filename: "dropbox-notes.txt"
   end
 
   def dropbox_auth_complete
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   get "ext/:user_uuid" => "extensions#show"
+
   post "ext/:user_uuid/push" => "extensions#push"
+  post "ext/:user_uuid/initial_sync" => "extensions#initial_sync"
+  get "ext/:user_uuid/download" => "extensions#download"
+
   get "dropbox_redirect" => "extensions#dropbox_auth_complete"
 
   root to: "dropbox#index"
